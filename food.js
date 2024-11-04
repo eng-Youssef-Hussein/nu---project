1e2b0003fdd6cb60114eee25ce63df9a357e104f
eng-Youssef-Hussein <youssefrtsdfg@gmail.com>
2024-11-04 07:57:24 +0200

    Add files via upload

---
+++ food.js
@@ -0,0 +1,8 @@
+//typing text code
+const typed = new Typed('.multiple-text', {
+    strings: ['Diet', 'Fat Lose', 'Make Your Food', 'Be Healthy',],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+  });
